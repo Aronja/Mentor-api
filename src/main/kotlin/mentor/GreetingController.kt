554d94000e00664140bcb7cb7c260757019bb6c1
Aronja <anovicky@thoughtworks.com>
2018-07-05 14:52:10 +0100

    add cross origin

--- src/main/kotlin/mentor/GreetingController.kt
+++ src/main/kotlin/mentor/GreetingController.kt
@@ -3,13 +3,18 @@ package mentor
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.CrossOrigin
+
 import java.util.concurrent.atomic.AtomicLong
 
+
 @RestController
+@CrossOrigin(maxAge = 3600)
 class GreetingController {
 
     val counter = AtomicLong()
 
+//    @CrossOrigin(origins = arrayOf("*"))
     @GetMapping("/greeting")
     fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
             Greeting(counter.incrementAndGet(), "Hello, $name")
